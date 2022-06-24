d1e355ae72d2653760d84d21e5359606556a1fc4
95twan <95twan@naver.com>
2022-06-25 00:52:05 +0900

    add validation orderDto

--- src/main/kotlin/com/baekseju/howmuch/pos/controller/OrderController.kt
+++ src/main/kotlin/com/baekseju/howmuch/pos/controller/OrderController.kt
@@ -5,6 +5,7 @@ import com.baekseju.howmuch.pos.dto.ResponseDto
 import com.baekseju.howmuch.pos.service.OrderService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/orders")
@@ -13,7 +14,7 @@ class OrderController(
 ) {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun addOrder(@RequestBody order: OrderDto): ResponseDto {
+    fun addOrder(@Valid @RequestBody order: OrderDto): ResponseDto {
         val orderDto = orderService.addOrder(order)
         return ResponseDto(HttpStatus.CREATED.value(), "주문 완료", orderDto)
     }
--- src/main/kotlin/com/baekseju/howmuch/pos/dto/OrderDto.kt
+++ src/main/kotlin/com/baekseju/howmuch/pos/dto/OrderDto.kt
@@ -1,12 +1,19 @@
 package com.baekseju.howmuch.pos.dto
 
 import java.time.Instant
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 class OrderDto(
     val id: Int? = null,
-    var menus: String,
-    var price: Int,
-    var payWith: String,
+    @field:NotBlank(message = "한 글자 이상 입력해야 합니다.")
+    var menus: String?,
+    @field:NotNull(message = "한 글자 이상 입력해야 합니다.")
+    @field:Min(value = 0L, message = "0 이상 입력해야 합니다.")
+    var price: Int?,
+    @field:NotBlank(message = "한 글자 이상 입력해야 합니다.")
+    var payWith: String?,
     var createdAt: Instant? = null,
     var updatedAt: Instant? = null,
     var deletedAt: Instant? = null
